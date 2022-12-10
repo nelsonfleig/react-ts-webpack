084a216a15e133b2647449709f7c53be2a2c621f
nelsonfleig <nfleig@hotmail.com>
2022-12-10 17:17:47 +0100

    add css, font and svg loaders

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import './styles.css';
+import IMAGE_PNG from './react-logo.png';
+import IMAGE_SVG from './react-logo.svg';
 
 export const App = () => {
-  return <h1>React Typescript Webpack Template</h1>;
+  return (
+    <>
+      <h1>React Typescript Webpack Template</h1>
+      <img src={IMAGE_PNG} alt='React logo png' width='300' height='300' />
+      <img src={IMAGE_SVG} alt='React logo svg' width='300' height='300' />
+    </>
+  );
 };
 
 export default App;
